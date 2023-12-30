64257c9549aec6c7628bb2f0c47cc8c9af96f8c1
Raphael Höser <raphael@hoeser.info>
2023-12-30 23:08:32 +0100

    Add some logging for deploy info

--- src/_data/isPreview.js
+++ src/_data/isPreview.js
@@ -1,2 +1,5 @@
+console.log("Run Mode: ", data.eleventy.env.runMode);
+console.log("Branch: ", data.eleventy.env.BRANCH);
+
 module.exports = (data) =>
   data.eleventy.env.runMode === "serve" || data.eleventy.env.BRANCH !== "main";
