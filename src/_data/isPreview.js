870d9093586a8e706840996e79ce0f7c634daa2e
Raphael Höser <raphael@hoeser.info>
2023-12-31 10:04:53 +0100

    fix preview banner
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- src/_data/isPreview.js
+++ src/_data/isPreview.js
@@ -1,16 +1,2 @@
-module.exports = (data) => {
-  console.log(
-    "Run Mode: ",
-    data.eleventy.env.runMode,
-    data.eleventy.env.runMode === "serve"
-  );
-  console.log(
-    "Branch: ",
-    data.eleventy.env.BRANCH,
-    data.eleventy.env.BRANCH !== "main"
-  );
-
-  return (
-    data.eleventy.env.runMode === "serve" || data.eleventy.env.BRANCH !== "main"
-  );
-};
+module.exports = (data) => 
+    data.eleventy.env.runMode === "serve" || process.env.BRANCH !== "main"
